6d948db93d5b2ed8433deff1aaba8726ef8c9893
v1p3er <cybertechik@protonmail.com>
2024-07-10 11:28:44 +0330

    implemented database to show on start page

--- home/static/home/start.js
+++ home/static/home/start.js
@@ -1,21 +1,88 @@
-const neshanMap = new nmp_mapboxgl.Map({
-    mapType: nmp_mapboxgl.Map.mapTypes.neshanVector,
-    container: "map",
-    zoom: 11,
-    pitch: 0,
-    center: [51.389, 35.6892],
-    minZoom: 2,
-    maxZoom: 21,
-    trackResize: true,
-    mapKey: "web.d34f7dfd2b35494e9d2cd5ac15c786e7", // Get your own API Key on https://platform.neshan.org/panel
-    poi: true,
-    traffic: true,
-    mapTypeControllerOptions: {
-      show: true,
-      position: "bottom-left",
-    },
+document.addEventListener("DOMContentLoaded", () => {
+  const menuHeader1 = document.querySelector(".header1");
+  const menuHeader2 = document.querySelector(".header2");
+
+  menuHeader1.addEventListener("click", () => {
+    const contents1 = document.querySelector(".contents1");
+    const contents2 = document.querySelector(".contents2");
+
+    if (!contents1.classList.contains("show")) {
+      contents2.classList.remove("show");
+    }
+    contents1.classList.toggle("show");
   });
-  // add marker to map
-  var marker = new nmp_mapboxgl.Marker()
-    .setLngLat([51.391173, 35.700954])
-    .addTo(neshanMap);
+
+  menuHeader2.addEventListener("click", () => {
+    const contents1 = document.querySelector(".contents1");
+    const contents2 = document.querySelector(".contents2");
+
+    if (!contents2.classList.contains("show")) {
+      contents1.classList.remove("show");
+    }
+    contents2.classList.toggle("show");
+  });
+});
+const result = document.querySelector(".result");
+result.addEventListener("click", () => {
+  const table = document.querySelector(".table");
+
+  if (!table.classList.contains("show")) {
+    table.classList.remove("show");
+  }
+  table.classList.toggle("show");
+});
+
+// *******************************
+//  document.addEventListener('DOMContentLoaded', () => {
+//       const MenuHeader = document.querySelectorAll('.menu-header');
+
+//       MenuHeader.forEach(item => {
+//           item.addEventListener('click', () => {
+//               const subContent = item.nextElementSibling;
+//               const SubmitReset = document.querySelector('.submit-reset');
+//               if (subContent && subContent.classList.contains('sub-content') && SubmitReset.classList.contains('submit-reset')) {
+//                   subContent.classList.toggle('show');
+//                   SubmitReset.classList.toggle('show');
+//               }
+//           });
+//       });
+
+//   });
+// **************************************
+// const next = document.querySelector('.mantaghe');
+// next.addEventListener("click",()=>{
+//     let link = document.createElement('link');
+//     link.rel = 'stylesheet';
+//     link.type = 'text/css';
+//     link.href = 'start2.css';
+//     document.getElementsByTagName('HEAD')[0].appendChild(link);
+// })
+// .......................................................................!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// const next2 = document.querySelector('.khak' );
+// next2.addEventListener("click",()=>{
+//     alert("hi2")
+//     let link = document.createElement('link');
+//     link.rel = 'stylesheet';
+//     link.type = 'text/css';
+//     link.href = 'start2.css';
+//     document.getElementsByTagName('HEAD')[0].appendChild(link);
+// })
+// const next3 = document.querySelector('.natayej' );
+// next3.addEventListener("click",()=>{
+//     alert("hi")
+//     let link = document.createElement('link');
+//     link.rel = 'stylesheet';
+//     link.type = 'text/css';
+//     link.href = 'start3.css';
+//     document.getElementsByTagName('HEAD')[0].appendChild(link);
+// })
+// const next4 = document.querySelector('.daryaft' );
+// next4.addEventListener("click",()=>{
+//     alert("hi4")
+//     let link = document.createElement('link');
+//     link.rel = 'stylesheet';
+//     link.type = 'text/css';
+//     link.href = 'start5.css';
+//     document.getElementsByTagName('HEAD')[0].appendChild(link);
+// })
--- home/views.py
+++ home/views.py
@@ -1,8 +1,36 @@
 from django.shortcuts import render
+from.models import SoilSample
+from django.db.models import Q
+from django.db.models import F
 
+
+def start(request):
+    soil_samples = SoilSample.objects.all()
+    area_names = request.GET.getlist('area-names')
+    parameters = request.GET.getlist('parameters')
+
+    if area_names:
+        soil_samples = soil_samples.filter(name__in=area_names)
+
+    if parameters:
+        for param in parameters:
+            if param == 'caco3':
+                soil_samples = soil_samples.filter(caco3__isnull=False)
+            elif param == 'CEC':
+                soil_samples = soil_samples.filter(CEC__isnull=False)
+            elif param == 'ece':
+                soil_samples = soil_samples.filter(ece__isnull=False)
+            elif param == 'ph':
+                soil_samples = soil_samples.filter(ph__isnull=False)
+
+    # If no parameters are selected, show all parameters
+    if not parameters:
+        soil_samples = soil_samples.all()
+    else:
+        # Select only the columns that are specified in the parameters
+        soil_samples = soil_samples.values(*parameters)
+
+    return render(request, 'home/start/start.html', {'soil_samples': soil_samples})
 
 def home(request):
-    
     return render(request, 'home/abak.html')
-def start(request):
-    return render(request, 'home/start/start.html')
